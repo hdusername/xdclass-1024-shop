30dfae489dc56f49ff9bf91124f7fbd451f9a9ad
handi <1667311156@qq.com>
2023-09-02 18:08:05 +0800

    使用mybatisplusgenerator生成代码

---
+++ xdclass-user-service/src/main/java/net/xdclass/controller/UserController.java
@@ -0,0 +1,21 @@
+package net.xdclass.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author hd
+ * @since 2023-09-02
+ */
+@RestController
+@RequestMapping("/userDO")
+public class UserController {
+
+}
+
